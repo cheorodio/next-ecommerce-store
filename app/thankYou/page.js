9e28b4932701108db6f9b5e040b423dbf891cffa
cheorodio <cheorodio@gmail.com>
2023-05-21 17:10:01 +0200

    Add thank you page

---
+++ app/thankYou/page.js
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import mail from '../../public/images/received.png';
+import styles from './thankYou.module.scss';
+
+export default function thankYouPage() {
+  return (
+    <section className={styles.thankyouContainer}>
+      <h1>Thank you for your order</h1>
+      <div>
+        <Image src={mail} alt="mail icon" />
+        <p>
+          Your oder has been recieved and an email receipt has been sent to the
+          email address provided.
+        </p>
+      </div>
+    </section>
+  );
+}
