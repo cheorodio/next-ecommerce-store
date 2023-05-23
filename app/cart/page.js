11efad630f6d13d6dd6a32897274eb0abdcfd040
cheorodio <cheorodio@gmail.com>
2023-05-23 17:28:01 +0200

    Add cart table head

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,10 +1,22 @@
+// import Image from 'next/image';
 import Link from 'next/link';
+// import { getProductById } from '../../database/products';
 import styles from './cart.module.scss';
 
 export default function CartPage() {
   return (
     <div className={styles.cartContainer}>
-      <div className={styles.cartOverviewContainer} />
+      <div className={styles.cartOverviewContainer}>
+        <div className={styles.cartTableHead}>
+          <div>Product</div>
+          <div>Name</div>
+          <div>Price</div>
+          <div>Quantity</div>
+          <div>Total</div>
+          <div>Action</div>
+        </div>
+      </div>
+
       <div className={styles.cartTotalContainer}>
         <div className={styles.cartTotalCard}>
           <h3>Cart Total</h3>
@@ -18,7 +30,7 @@ export default function CartPage() {
             </Link>
             <Link
               className={`${styles.checkoutButton} ${styles.cartButton}`}
-              href="/#"
+              href="/checkout"
             >
               Checkout
             </Link>
