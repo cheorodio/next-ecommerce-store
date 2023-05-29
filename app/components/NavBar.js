d13ea4d8b653afda0c2bf9cc9b9af239c852076f
cheorodio <cheorodio@gmail.com>
2023-05-29 20:41:08 +0200

    Restyle navigation bar

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,54 +1,48 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { BsCart3 } from 'react-icons/bs';
+import { BsHandbag } from 'react-icons/bs';
+// import { GiHamburgerMenu } from 'react-icons/gi';
 import logo from '../../public/images/logo.png';
 import CartBadge from './CartBadge';
 import styles from './NavBar.module.scss';
 
 export default function NavBar() {
   return (
-    <header className={styles.navigationHeader}>
-      <div className={styles.logo}>
-        <Link href="/#" style={{ fontSize: '50px' }}>
-          <Image src={logo} alt="logo" height={40} width={70} />
-        </Link>
-      </div>
-
-      <div className={styles.navbar}>
-        <div className={styles.leftSide} />
-
-        <div className={styles.navigationBar}>
-          <div className={styles.navigationLinks}>
-            <Link className={styles.navigationLink} href="/#">
-              Home
-            </Link>
-            <Link
-              data-test-id="products-link"
-              className={styles.navigationLink}
-              href="/products"
-            >
-              Shop
-            </Link>
-            <Link className={styles.navigationLink} href="/story">
-              Our Story
-            </Link>
-            <Link className={styles.navigationLink} href="/blog">
-              Blog
-            </Link>
-          </div>
-        </div>
-
-        <div className={styles.cartContainer}>
+    <>
+      <div className={styles.topBanner}>Free worldwide shipping</div>
+      <header className={styles.navContainer}>
+        <div className={styles.navigationLinks}>
+          <Link className={styles.navigationLink} href="/#">
+            Home
+          </Link>
           <Link
-            href="/cart"
-            className={styles.cartIcon}
-            data-test-id="cart-link"
+            data-test-id="products-link"
+            className={styles.navigationLink}
+            href="/products"
           >
-            <BsCart3 style={{ fontSize: '1.25rem' }} />
-            <CartBadge data-test-id="cart-count" />
+            Shop
+          </Link>
+          <Link className={styles.navigationLink} href="/story">
+            Our Story
+          </Link>
+          <Link className={styles.navigationLink} href="/blog">
+            Blog
           </Link>
         </div>
-      </div>
-    </header>
+        <div className={styles.logoContainer}>
+          <Link href="/#">
+            <Image src={logo} alt="logo" height={50} width={80} />
+          </Link>
+        </div>
+        <div className={styles.cartContainer}>
+          <Link href="/cart" data-test-id="cart-link">
+            <BsHandbag className={styles.cartIcon} />
+            <div className={styles.badge}>
+              <CartBadge data-test-id="cart-count" />
+            </div>
+          </Link>
+        </div>
+      </header>
+    </>
   );
 }
