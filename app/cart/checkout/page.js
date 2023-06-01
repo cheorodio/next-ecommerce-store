27295528f7e19d0e3890e711c05952d3f6e1bebe
cheorodio <cheorodio@gmail.com>
2023-06-01 17:36:43 +0200

    Move form and summary into their own components

--- app/cart/checkout/page.js
+++ app/cart/checkout/page.js
@@ -1,5 +1,12 @@
+import styles from './checkout.module.scss';
 import CheckOutForm from './CheckOutForm';
+import OrderSummary from './OrderSummary';
 
 export default function CheckoutPage() {
-  return <CheckOutForm />;
+  return (
+    <main className={styles.checkoutContainer}>
+      <CheckOutForm />
+      <OrderSummary />
+    </main>
+  );
 }
