6e67d18f8e7f1cb22ded10c2a8241231da797475
cheorodio <cheorodio@gmail.com>
2023-06-07 22:20:01 +0200

    Setup deployment on fly.io

--- database/connect.ts
+++ database/connect.ts
@@ -2,8 +2,8 @@ import 'server-only'; // 'poison' our code so we dont accidentally run it in the
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
 
-config(); // run this in order to have access to environment variables
-
+// config(); // run this in order to have access to environment variables
+if (!process.env.FLY_IO) config();
 // connection
 export const sql = postgres({
   // this transforms everything to camelCase and all undefined value to null so it doesn't cause conflicts
