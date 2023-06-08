4686037a897e2b9520d1d3cb8f312c73531d9143
cheorodio <cheorodio@gmail.com>
2023-06-08 12:49:25 +0200

    Continue with deployment on fly.io

--- database/connect.ts
+++ database/connect.ts
@@ -1,14 +1,48 @@
-import 'server-only'; // 'poison' our code so we dont accidentally run it in the frontend
+import 'server-only';
 import { config } from 'dotenv-safe';
-import postgres from 'postgres';
+import { headers } from 'next/headers';
+import postgres, { Sql } from 'postgres';
 
-// config(); // run this in order to have access to environment variables
+// This loads all environment variables from a .env file
+// for all code after this line
 if (!process.env.FLY_IO) config();
-// connection
-export const sql = postgres({
-  // this transforms everything to camelCase and all undefined value to null so it doesn't cause conflicts
-  transform: {
-    ...postgres.camel,
-    undefined: null,
-  },
-});
+
+declare module globalThis {
+  let postgresSqlClient: Sql;
+}
+
+// Connect only once to the database
+// https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
+function connectOneTimeToDatabase() {
+  if (!('postgresSqlClient' in globalThis)) {
+    globalThis.postgresSqlClient = postgres({
+      transform: {
+        ...postgres.camel,
+        undefined: null,
+      },
+    });
+  }
+
+  // Workaround to force Next.js Dynamic Rendering:
+  //
+  // Wrap sql`` tagged template function to call `headers()` from
+  // next/headers before each database query. `headers()` is a
+  // Next.js Dynamic Function, which causes the page to use
+  // Dynamic Rendering.
+  //
+  // https://nextjs.org/docs/app/building-your-application/rendering/static-and-dynamic-rendering
+  //
+  // Ideally there would something built into Next.js for this,
+  // which has been requested here:
+  //
+  // https://github.com/vercel/next.js/discussions/50695
+  return ((
+    ...sqlParameters: Parameters<typeof globalThis.postgresSqlClient>
+  ) => {
+    headers();
+    return globalThis.postgresSqlClient(...sqlParameters);
+  }) as typeof globalThis.postgresSqlClient;
+}
+
+// Connect to PostgreSQL
+export const sql = connectOneTimeToDatabase();
