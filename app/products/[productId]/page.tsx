271c7dacb1db5ddb8ba9cdfed11067f5602d291e
cheorodio <cheorodio@gmail.com>
2023-06-10 11:47:40 +0200

    Fix drone error

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -48,7 +48,7 @@ export default async function SingleProductPage(props: Props) {
         <div className={styles.productInfoContainer}>
           <h1>{singleProduct.name}</h1>
           <h5>{singleProduct.description}</h5>
-          <h6 data-test-id="product-price">{singleProduct.price} EUR</h6>
+          <h6 data-test-id="product-price"> {singleProduct.price} EUR</h6>
           <p className={styles.quantityTitle}>Quantity</p>
           <div>
             <AddToCart productId={singleProduct.id} />
