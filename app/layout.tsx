90ec9f692e9f66bd88b1189f71ebf96c76b0e181
cheorodio <cheorodio@gmail.com>
2023-06-10 20:16:51 +0200

    Remove font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.scss';
-import { Poppins } from 'next/font/google';
+// import { Caveat, Poppins } from 'next/font/google';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 
-const poppins = Poppins({ subsets: ['latin'], weight: '400' });
+// const poppins = Poppins({ subsets: ['latin'], weight: '400' });
+// const caveat = Caveat({ subsets: ['latin'], weight: '400' });
 
 export const metadata = {
   title: { default: 'Vida | Handmade Jewellery' },
@@ -17,7 +18,7 @@ type LayoutProps = {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body>
         <NavBar />
         {children}
         <Footer />
