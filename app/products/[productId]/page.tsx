13bafd4ae34c05aaf020a19547c1d3e01da4b4c9
cheorodio <cheorodio@gmail.com>
2023-09-20 10:08:39 +0200

    Edit mobile media query for single product page

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -33,24 +33,24 @@ export default async function SingleProductPage(props: Props) {
   return (
     <main>
       <section className={styles.productContainer}>
-        {/* <div> */}
-        <Image
-          data-test-id="product-image"
-          src={`/images/products/${singleProduct.name}.avif`}
-          width={500}
-          height={500}
-          className={styles.productImage}
-          alt={singleProduct.name}
-          // priority={true}
-          unoptimized={true}
-        />
-        {/* </div> */}
+        <div className={styles.imageContainer}>
+          <Image
+            data-test-id="product-image"
+            src={`/images/products/${singleProduct.name}.avif`}
+            width={500}
+            height={500}
+            className={styles.productImage}
+            alt={singleProduct.name}
+            // priority={true}
+            unoptimized={true}
+          />
+        </div>
         <div className={styles.productInfoContainer}>
           <h1>{singleProduct.name}</h1>
           <h5>{singleProduct.description}</h5>
-          <h6 data-test-id="product-price">{singleProduct.price} EUR</h6>
+          <h6 data-test-id="product-price"> €{singleProduct.price}</h6>
           <p className={styles.quantityTitle}>Quantity</p>
-          <div>
+          <div className={styles.quantityButtons}>
             <AddToCart productId={singleProduct.id} />
           </div>
         </div>
